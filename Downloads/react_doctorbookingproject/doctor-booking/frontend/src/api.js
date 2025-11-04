d7c2cc5502bc520c2df11eea88744d3febd631ff
Supriya Nerella <113848392+supriya869@users.noreply.github.com>
2025-11-04 16:10:05 +0530

    Update api.js

--- Downloads/react_doctorbookingproject/doctor-booking/frontend/src/api.js
+++ Downloads/react_doctorbookingproject/doctor-booking/frontend/src/api.js
@@ -1,7 +1,15 @@
+//import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:8035/api",
+// });
+
+// export default api;  // ✅ must be a default export
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8035/api",
+  baseURL: "https://reactdoctorbookingapp.onrender.com/api",
 });
 
-export default api;  // ✅ must be a default export
+export default api;
+
